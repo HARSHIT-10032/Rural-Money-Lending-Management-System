40918454b51bb3db739464932fa26b8addda7ead
HARSHIT-10032 <harshitpatel9398@gmail.com>
2025-05-09 22:54:31 +0530

    add Routes: authRoutes , update index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const connectDB = require("./config/db");
 const app = express();
 app.use(express.json());
 
+const authRoutes = require("./routes/authRoutes");
+
+// routes
+app.use("/api/auth", authRoutes);
+
 // Start server
 connectDB().then(() => {
   app.listen(5000, () => console.log("Server running on port 5000"));
