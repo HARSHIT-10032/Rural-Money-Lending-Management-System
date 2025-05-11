71b54f559ee24d7450059f2ece5e5c82625a2568
HARSHIT-10032 <harshitpatel9398@gmail.com>
2025-05-11 17:58:39 +0530

    update fetchLoanController : getPendingLoans & Routes: loanRoutes.js

--- server/controllers/fetchLoanController.js
+++ server/controllers/fetchLoanController.js
@@ -10,3 +10,23 @@ exports.getAllLoans = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+// Manage Interest API
+exports.getPendingLoans = async (req, res) => {
+  try {
+    const loans = await Loan.find({
+      status: { $ne: "Cleared" },
+      totalInterest: { $gt: 0 }
+    }).populate(
+      "user",
+      "name surname mobile village district tehsil state"
+    );
+
+    console.log(" Pending Loans Count:", loans.length);
+
+    return res.status(200).json(loans);
+  } catch (err) {
+    console.error("❌ getPendingLoans error:", err);
+    return res.status(500).json({ message: err.message });
+  }
+};
--- server/routes/loanRoutes.js
+++ server/routes/loanRoutes.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const { createLoan } = require("../controllers/loanController");
-const { getAllLoans } = require("../controllers/fetchLoanController");
+const { 
+    getAllLoans,
+    getPendingLoans,
+} = require("../controllers/fetchLoanController");
 
 // Create Loan API to create loan for user
 router.post("/create", createLoan);
+
+// == MANAGE INTEREST API
+router.get("/pending", getPendingLoans);
 
 // == All loans Dashboard API
 router.get("/", getAllLoans);
