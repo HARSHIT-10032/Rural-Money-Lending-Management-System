d6a6e51370ee0a2ed0e912c98962e37098222e8c
HARSHIT-10032 <harshitpatel9398@gmail.com>
2025-05-13 20:15:44 +0530

    installed Cors and updated Index.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,23 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
 const errorHandler = require("./middlewares/errorHandler");
-
-const app = express();
-app.use(express.json());
-app.use(errorHandler);
 
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 const loanRoutes = require("./routes/loanRoutes");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/loans", loanRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(errorHandler);
 
 // Start server
 connectDB().then(() => {
